64837cbf9c2998e076482293863542a18b8744b3
4lexks <99203252+4lexks@users.noreply.github.com>
2026-02-22 12:37:01 +0100

    Updated footer text

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -14,13 +14,13 @@ export const Footer = ({activeApp} : FooterApp) => {
             <p>This project was made by Emma Karnbrock, Ronja Laurila, Isha Triloki and Aleksandra Čaluković for course MAM05. The code for this project can be found <a href="https://github.com/4lexks/MAM05" target="_blank" className="font-bold text-blue-500 hover:text-blue-700">here</a>.</p>
             {/*Shows Different text based on the active page*/}
                 {activeApp === "habitTracker" && (
-                    <div>The habit tracker uses the Hasura API to create, show and remove habit cards.</div>
+                    <div>The habit tracker uses the Hasura API to create, show and remove habit cards. Local Storage is used to remember the state of the checkboxes.</div>
                 )}
                 {activeApp === "medicineTracker" && (
-                    <div>The medicine tracker uses the Hasure API to create, show and remove medication cards. The autofill function in the form to add a medication used the Dutch geneesmiddeleninformatiebank.</div>
+                    <div>The medicine tracker uses the Hasure API to create, show and remove medication cards. The autofill function in the form to add a medication used the Dutch geneesmiddeleninformatiebank. Local Storage is used to remember the state of the checkboxes.</div>
                 )}
                 {activeApp === "heartRateMonitor" && (
-                    <div>The data shown in the graphs was exported from a Garmin vivoactive 6 smartwatch. Python was used to converted the files from fit-format to csv-format, and synthesised using the library synthetic data vault (SDV).</div>
+                    <div>The data shown in the graphs was exported from a Garmin vivoactive 6 smartwatch. Python was used to manipulate the data, synthesise it using the library Synthetic Data Vault, and convert it to .csv-format.</div>
                 )}
         </div>
     );
